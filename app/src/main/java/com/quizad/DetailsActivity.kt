98d13faf8d0aed2e4d361994549c324d63ae19b0
talhaurganci <65421059+talhaurganci@users.noreply.github.com>
2022-04-05 05:39:32 +0300

    Login operations

---
+++ app/src/main/java/com/quizad/DetailsActivity.kt
@@ -0,0 +1,34 @@
+package com.quizad
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        val googleId = intent.getStringExtra("google_id")
+        val googleFirstName = intent.getStringExtra("google_first_name")
+        val googleLastName = intent.getStringExtra("google_last_name")
+        val googleEmail = intent.getStringExtra("google_email")
+        val googleProfilePicURL = intent.getStringExtra("google_profile_pic_url")
+        val googleAccessToken = intent.getStringExtra("google_id_token")
+
+        val googleIdTextView = findViewById<TextView>(R.id.google_id_textview)
+        val googleFirstNameTextView = findViewById<TextView>(R.id.google_first_name_textview)
+        val googleLastNameTextView = findViewById<TextView>(R.id.google_last_name_textview)
+        val googleEmailTextView = findViewById<TextView>(R.id.google_email_textview)
+        val googleProfilePicTextView = findViewById<TextView>(R.id.google_profile_pic_textview)
+        val googleIdTokenTextView = findViewById<TextView>(R.id.google_id_token_textview)
+
+        googleIdTextView.text = googleId
+        googleFirstNameTextView.text = googleFirstName
+        googleLastNameTextView.text = googleLastName
+        googleEmailTextView.text = googleEmail
+        googleProfilePicTextView.text = googleProfilePicURL
+        googleIdTokenTextView.text = googleAccessToken
+
+    }
+}
--- app/src/main/java/com/quizad/MainActivity.kt
+++ app/src/main/java/com/quizad/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.quizad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,11 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buton = findViewById<Button>(R.id.button)
+        val buton2 = findViewById<Button>(R.id.button2)
 
         buton.setOnClickListener {
             Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
             connection()
             showUsers()
+        }
+
+        buton2.setOnClickListener {
+            Intent(this, Login::class.java).apply {
+                startActivity(this)
+            }
         }
     }
 }
