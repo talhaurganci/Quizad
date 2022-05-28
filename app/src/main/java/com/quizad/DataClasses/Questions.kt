96b9d4734db6fb86d7ed127a1a3440d175d09fd5
talhaurganci <65421059+talhaurganci@users.noreply.github.com>
2022-05-28 17:44:56 +0300

    Working with JSON objects

--- app/src/main/java/com/quizad/DataClasses/Questions.kt
+++ app/src/main/java/com/quizad/DataClasses/Questions.kt
@@ -4,7 +4,7 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
-object Questions : Table<Nothing>("Questions"){
+object Questions : Table<Nothing>("questions"){
     val id = int("id").primaryKey()
     val question_text = varchar("question_text")
     val difficulty = int("difficulty")
--- app/src/main/java/com/quizad/QuestionAnswerActivity.kt
+++ app/src/main/java/com/quizad/QuestionAnswerActivity.kt
@@ -5,18 +5,20 @@ import android.os.StrictMode
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.quizad.DataClasses.AnswersMapper
 import com.quizad.DataClasses.Question_Options
 import com.quizad.DataClasses.Questions
-import com.quizad.DataClasses.Users
 import org.ktorm.database.Database
-import org.ktorm.dsl.from
-import org.ktorm.dsl.select
-import java.sql.DriverManager
-import java.sql.SQLException
+import org.ktorm.dsl.*
+import org.ktorm.jackson.KtormModule
 
 class QuestionAnswerActivity : AppCompatActivity() {
+
+    var difficulty = 1
+    var questionCount = 0
+    var questionsIdList: MutableList<Int> = arrayListOf()
+    var random_id: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +30,36 @@ class QuestionAnswerActivity : AppCompatActivity() {
         val option3 = findViewById<Button>(R.id.option3)
         val option4 = findViewById<Button>(R.id.option4)
 
-        fun test() {
+        /* fun getQuestions() {
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+
+            val jdbcUrl = "jdbc:mariadb://server.pinet.com.tr:3306/quizad_com"
+            val database = Database.connect(
+                    jdbcUrl,
+                    driver = "com.mysql.jdbc.Driver",
+                    user = "quizad_com",
+                    password = "4)rY@8)5QXSAHpjH"
+            )
+
+            for (row in database.from(Questions).select().where { (Questions.difficulty eq 1) }) {
+                val id: Int? = row[Questions.id]
+                val questionText = row[Questions.question_text]
+                val difficulty = row[Questions.difficulty]
+
+                if (id != null) {
+                    questionsList.add(id)
+                    if (questionText != null) {
+                        questionsList.add(questionText)
+                        if (difficulty != null) {
+                            questionsList.add(difficulty)
+                        }
+                    }
+                }
+            }
+        }*/
+
+        /*fun getQuestionOptions() {
             val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
             StrictMode.setThreadPolicy(policy)
 
@@ -43,50 +74,118 @@ class QuestionAnswerActivity : AppCompatActivity() {
             for (row in database.from(Question_Options).select()) {
                 val id: Int? = row[Question_Options.question_id]
                 val answers: List<AnswersMapper>? = row[Question_Options.answers]
-                println("$id, $answers")
-            }
-        }
 
-        /*fun getQuestionText() {
+                //println(row[Question_Options.question_id])
+
+                if (id != null) {
+                    questionOptionsList.add(id)
+                    if (answers != null) {
+                        questionOptionsList.addAll(answers)
+                    }
+                }
+                //println(mergedList)
+            }
+        }*/
+
+        fun test() {
             val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
             StrictMode.setThreadPolicy(policy)
-            try {
-                val jdbcUrl = "jdbc:mariadb://server.pinet.com.tr:3306/quizad_com"
-                val connection = DriverManager.getConnection(jdbcUrl, "quizad_com", "4)rY@8)5QXSAHpjH")
 
-                println(connection.isValid(0))
+            val jdbcUrl = "jdbc:mariadb://server.pinet.com.tr:3306/quizad_com"
+            val database = Database.connect(
+                    jdbcUrl,
+                    driver = "com.mysql.jdbc.Driver",
+                    user = "quizad_com",
+                    password = "4)rY@8)5QXSAHpjH"
+            )
 
-                val getQTextQuery = connection.prepareStatement("SELECT * FROM questions WHERE id=1")
-                //val getOpTextQuery = connection.prepareCall("SELECT * FROM question_options WHERE question_id=1")
-                val questions = mutableListOf<Questions>()
+            for (row in database.from(Questions).select(Questions.id).where { (Questions.difficulty eq difficulty) }) {
+                val questionid: Int? = row[Questions.id]
 
-                val QTextResult = getQTextQuery.executeQuery()
-               // val OpTextResult = getOpTextQuery.executeQuery()
+                if (questionid != null) {
+                    questionsIdList.add(questionid)
 
-               while (QTextResult.next()) {
-                   val id = QTextResult.getInt("id")
-                   val questiontext = QTextResult.getString("question_text")
-                   val difficulty = QTextResult.getInt("difficulty")
-                   questions.add(Questions(id,questiontext,difficulty))
-                   question_text.setText(questiontext)
-                   println(questions)
-               }
+                }
 
-               /* while (OpTextResult.next()) {
-                    val optiontext = QTextResult.getString("answers")
-                    option1.setText(optiontext)
+
+                random_id = questionsIdList.random()
+
+                /*for (row in database.from(Question_Options).select().where { Question_Options.question_id eq 1 }) {
+
+                    val id: Int? = row[Question_Options.question_id]
+                    val answers: List<AnswersMapper>? = row[Question_Options.answers]
+
+                    //println(row[Question_Options.question_id])
+
+                    if (id != null) {
+                        questionOptionsList.add(id)
+                        if (answers != null) {
+                            questionOptionsList.addAll(answers)
+                        }
+                    }
+                    //println(mergedList)
+
                 }*/
-            } catch (e: SQLException) {
-                e.printStackTrace()
+            }
+            questionCount++
+            // println(questionCount)
+            //println(questionsList.random())
+            //println(questionsList)
+        }
+
+        fun test2() {
+            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+
+            val jdbcUrl = "jdbc:mariadb://server.pinet.com.tr:3306/quizad_com"
+            val database = Database.connect(
+                    jdbcUrl,
+                    driver = "com.mysql.jdbc.Driver",
+                    user = "quizad_com",
+                    password = "4)rY@8)5QXSAHpjH"
+            )
+
+            for (row in database.from(Questions).select().where { (Questions.id eq random_id) }) {
+                val questionid: Int? = row[Questions.id]
+                val questionText = row[Questions.question_text]
+                val difficulty = row[Questions.difficulty]
+
+                question_text.setText(questionText)
+
+                //println("$questionid, $questionText, $difficulty")
+
+                for (row in database.from(Question_Options).select().where { Question_Options.question_id eq random_id }) {
+
+
+                    val id: Int? = row[Question_Options.question_id]
+                    //val answers: List<AnswersMapper>? = row[Question_Options.answers]
+                    val answers = row[Question_Options.answers] ?: emptyList()
+
+                    //println(row[Question_Options.question_id])
+
+                    println(answers)
+/*
+                    option1.setText(answers?.get(0)?.toString())
+                    option2.setText(answers?.get(1)?.toString())
+                    option3.setText(answers?.get(2)?.toString())
+                    option4.setText(answers?.get(3)?.toString())
+                    //println(mergedList)
+*/
+                    option1.text = answers[0].tr
+                    option2.text = answers[1].tr
+                    option3.text = answers[2].tr
+                    option4.text = answers[3].tr
+
+                }
+
             }
         }
-*/
-        /*fun getQuestionOptions() {
-
-        }
-*/
-        //getQuestionText()
+        //getQuestions()
+        //getQuestionOptions()
         test()
+        //println(questionsIdList+"asd")
+        test2()
+        //println(questionsIdList)
     }
 }
 
